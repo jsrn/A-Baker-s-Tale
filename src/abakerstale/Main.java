3ec49541db923b6f883dd656c47c655bc3342c7d
James Nutt <james.srn@gmail.com>
2012-04-01 17:22:57 +0100

    first commit

---
+++ src/abakerstale/Main.java
@@ -0,0 +1,16 @@
+package abakerstale;
+
+/**
+ *
+ * @author James
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+}
---
+++ src/mobiles/Mobile.java
@@ -0,0 +1,8 @@
+package mobiles;
+
+/**The base clase for mobiles.
+ * @author James
+ */
+public class Mobile {
+
+}
