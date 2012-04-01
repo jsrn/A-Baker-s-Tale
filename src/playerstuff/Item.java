f7fffb8425e95feb4e4522461d39b6e6ee74564d
James Nutt <james.srn@gmail.com>
2012-04-01 19:53:06 +0100

    add quantity to item

--- src/playerstuff/Item.java
+++ src/playerstuff/Item.java
@@ -7,11 +7,22 @@ package playerstuff;
 public class Item {
 
     private String name;
-    private int quantity;
+    private int quantity = 0;
 
     public Item(String name, int quantity){
         this.name = name;
         this.quantity = quantity;
     }
 
+    public String getName(){
+        return name;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+
+    public void addQuantity(int quantity){
+        quantity += Math.abs(quantity);
+    }
 }
