759751d4f471fc087800f78c0a480a008ad54412
James Nutt <james.srn@gmail.com>
2012-04-02 20:35:36 +0100

    main method change

--- src/abakerstale/Main.java
+++ src/abakerstale/Main.java
@@ -1,9 +1,11 @@
 package abakerstale;
 
+import gui.GameScreen;
 import gui.GameTest;
 import mobiles.Player;
 import playerstuff.Inventory;
 import playerstuff.Item;
+import world.World;
 
 /**
  *
@@ -15,7 +17,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        GameTest g = new GameTest();
-        g.setVisible(true);
+        World w = new World(5);
+        GameScreen g = new GameScreen(w);
     }
 }
