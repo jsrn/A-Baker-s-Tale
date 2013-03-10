728b18a77a687df360a11ae4a9332b8ac1807f8b
James Nutt <james.srn@gmail.com>
2013-03-10 15:57:40 +0000

    a class containing the games constant variables

---
+++ src/abakerstale/Constants.java
@@ -0,0 +1,13 @@
+package abakerstale;
+
+public class Constants
+{
+	// Keycodes
+        // Directions
+	public static final int LEFT = 37;
+	public static final int UP = 38;
+	public static final int RIGHT = 39;
+	public static final int DOWN = 40;
+        // Letters
+        public static final int I = 73;
+}
