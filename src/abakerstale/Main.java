60614d5989fc09bb712258010fa5a4344fed68dc
James Nutt <james.srn@gmail.com>
2013-03-10 15:58:11 +0000

    nvc

--- src/abakerstale/Main.java
+++ src/abakerstale/Main.java
@@ -1,7 +1,6 @@
 package abakerstale;
 
 import gui.GameScreen;
-import gui.GameTest;
 import mobiles.Player;
 import playerstuff.Inventory;
 import playerstuff.Item;
@@ -17,7 +16,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        World w = new World(5);
+        World w = new World(20,15);
         GameScreen g = new GameScreen(w);
     }
 }
