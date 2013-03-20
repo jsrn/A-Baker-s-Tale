08fffbb565683a7c6dded5c7c509cd184eeabebe
James Nutt <james.srn@gmail.com>
2013-03-20 15:57:35 +0000

    better system to determine which sprite gets rendered

--- mobiles/Player.java
+++ mobiles/Player.java
@@ -1,5 +1,6 @@
 package mobiles;
 
+import abakerstale.Constants;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import javax.imageio.ImageIO;
@@ -7,14 +8,14 @@ import javax.imageio.ImageIO;
 public class Player extends Humanoid {
 
     BufferedImage playerSprites;
-
     int SOUTH_ROWHEIGHT = 0;
     int EAST_ROWHEIGHT = 26;
     int NORTH_ROWHEIGHT = 52;
     int WEST_ROWHEIGHT = 78;
-    
     int STEP1_OFFSET = 0;
     int STEP2_OFFSET = 26;
+    int STEP3_OFFSET = 52;
+    int STEP4_OFFSET = 78;
 
     public Player(int id, int x, int y) {
         super(id, x, y);
@@ -26,18 +27,44 @@ public class Player extends Humanoid {
     }
 
     public BufferedImage getImage() {
-        switch(getDirection()){
-            case 1:
-                return playerSprites.getSubimage(STEP1_OFFSET, NORTH_ROWHEIGHT, 25, 25);
-            case 2:
-                return playerSprites.getSubimage(STEP1_OFFSET, EAST_ROWHEIGHT, 25, 25);
-            case 3:
-                return playerSprites.getSubimage(STEP1_OFFSET, SOUTH_ROWHEIGHT, 25, 25);
-            case 4:
-                return playerSprites.getSubimage(STEP1_OFFSET, WEST_ROWHEIGHT, 25, 25);
-            default:
-                return playerSprites.getSubimage(STEP1_OFFSET, SOUTH_ROWHEIGHT, 25, 25);
-        }
+        return playerSprites.getSubimage(getStepOffset(), getDirectionOffset(), 25, 25);
     }
-        
+
+    private int getDirectionOffset() {
+        int offset = SOUTH_ROWHEIGHT;
+
+        switch (getDirection()) {
+            case Constants.DIRECTION_NORTH:
+                offset = NORTH_ROWHEIGHT;
+            case Constants.DIRECTION_EAST:
+                return EAST_ROWHEIGHT;
+            case Constants.DIRECTION_SOUTH:
+                return SOUTH_ROWHEIGHT;
+            case Constants.DIRECTION_WEST:
+                return WEST_ROWHEIGHT;
+        }
+
+        return offset;
+    }
+
+    private int getStepOffset() {
+        int offset = STEP1_OFFSET;
+
+        switch (getStep()) {
+            case 1:
+                offset = STEP1_OFFSET;
+                break;
+            case 2:
+                offset = STEP2_OFFSET;
+                break;
+            case 3:
+                offset = STEP3_OFFSET;
+                break;
+            case 4:
+                offset = STEP4_OFFSET;
+                break;
+        }
+
+        return offset;
+    }
 }
