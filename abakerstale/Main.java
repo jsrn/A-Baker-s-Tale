e79a9b56abbcc2d7ec0a5df48c868b27f03d3e07
James Nutt <james.srn@gmail.com>
2013-04-26 13:17:36 +0100

    removed logging

--- abakerstale/Main.java
+++ abakerstale/Main.java
@@ -4,10 +4,27 @@ import gui.GameJFrame;
 import world.World;
 
 public class Main {
-    
-    public Main(){
+
+    public Main() {
         Globals.WORLD = new World();
         GameJFrame gameJFrame = new GameJFrame();
+
+        while (true) {
+            try {
+                Thread.sleep(1000);
+                updateGame();
+                displayGame();
+            } catch (InterruptedException ex) {
+                System.err.println("Something happened!");
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    private void updateGame() {
+    }
+
+    private void displayGame() {
     }
 
     public static void main(String[] args) {
