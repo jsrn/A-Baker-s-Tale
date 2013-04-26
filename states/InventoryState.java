b3eb70d569c9dbcdb3431078c0cfcfa8941953db
James Nutt <james.srn@gmail.com>
2013-04-26 15:21:27 +0100

    moved state responsibility for keypresses and rendering to these classes

--- states/InventoryState.java
+++ states/InventoryState.java
@@ -1,8 +1,42 @@
 package states;
 
+import abakerstale.Constants;
+import abakerstale.Globals;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
 public class InventoryState extends MenuState {
+
     @Override
-    public void Render(){
-        System.out.println("INVENTORY OPEN");
+    public void Render() {
+        BufferedImage frame = new BufferedImage(600, 400, BufferedImage.TYPE_INT_ARGB);
+        Graphics frameGraphics = frame.getGraphics();
+        
+        Graphics2D g2d = (Graphics2D) frameGraphics;
+        // Background
+        g2d.setColor(Constants.GUI_BACKGROUND);
+        g2d.fillRect(25, 25, 450, 325);
+        // Border
+        g2d.setColor(Constants.GUI_BORDER);
+        g2d.drawRect(25, 25, 450, 325);
+        g2d.drawLine(25, 45, 475, 45);
+        // Labels
+        g2d.setColor(Constants.GUI_TEXT);
+        frameGraphics.drawString("Inventory", 30, 40);
+        
+        Globals.gamePanel.getGraphics().drawImage(frame, 0, 0, null);
+    }
+
+    @Override
+    public void Keypress(int keycode) {
+        switch (keycode) {
+            case Constants.KEY_ESC: // up
+                Globals.stateMachine.Change("localmap");
+                break;
+            default:
+                System.out.println("Unregistered keycode: " + keycode);
+                break;
+        }
     }
 }
--- states/LocalState.java
+++ states/LocalState.java
@@ -1,11 +1,36 @@
 package states;
 
-public class LocalState implements IState{
+import abakerstale.Constants;
+import abakerstale.Globals;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import world.Cell;
+
+public class LocalState implements IState {
 
     public void Update(float elapsedTime) {
     }
 
     public void Render() {
+        BufferedImage frame = new BufferedImage(600, 400, BufferedImage.TYPE_INT_ARGB);
+        Graphics frameGraphics = frame.getGraphics();
+        int tilewidth = Constants.TILE_WIDTH;
+        Cell[][] cells = Globals.WORLD.getCurrentScreen().getCells();
+        for (int x = 0; x < 20; x++) {
+            for (int y = 0; y < 15; y++) {
+
+                frameGraphics.drawImage(cells[x][y].getImage(), x * tilewidth, y * tilewidth, null);
+
+                if (cells[x][y].hasItem()) {
+                    frameGraphics.drawImage(cells[x][y].getItem().getImage(), x * tilewidth, y * tilewidth, null);
+                }
+
+                if (x == Globals.PLAYER.getX() && y == Globals.PLAYER.getY()) {
+                    frameGraphics.drawImage(Globals.PLAYER.getImage(), x * tilewidth, y * tilewidth, null);
+                }
+            }
+        }
+        Globals.gamePanel.getGraphics().drawImage(frame, 0, 0, null);
     }
 
     public void OnEnter() {
@@ -13,5 +38,68 @@ public class LocalState implements IState{
 
     public void OnExit() {
     }
-    
+
+    public void Keypress(int keycode) {
+        switch (keycode) {
+            case Constants.KEY_UP:
+                Globals.PLAYER.setDirection(Constants.DIRECTION_NORTH);
+                Globals.PLAYER.goNorth(1);
+                break;
+            case Constants.KEY_DOWN:
+                Globals.PLAYER.setDirection(Constants.DIRECTION_SOUTH);
+                Globals.PLAYER.goSouth(1);
+                break;
+            case Constants.KEY_LEFT:
+                Globals.PLAYER.setDirection(Constants.DIRECTION_WEST);
+                Globals.PLAYER.goWest(1);
+                break;
+            case Constants.KEY_RIGHT:
+                Globals.PLAYER.setDirection(Constants.DIRECTION_EAST);
+                Globals.PLAYER.goEast(1);
+                break;
+            case Constants.KEY_I:
+                Globals.stateMachine.Change("inventorystate");
+                break;
+            case Constants.KEY_SPACE:
+                playerAction();
+                break;
+            case Constants.KEY_ENTER:
+                playerAction();
+                break;
+            default:
+                System.out.println("Unregistered keycode: " + keycode);
+                break;
+        }
+    }
+
+    private void playerAction() {
+        System.out.println("Performing player action");
+        Cell[][] cells = Globals.WORLD.getCurrentScreen().getCells();
+        int direction = Globals.PLAYER.getDirection();
+        int playerX = Globals.PLAYER.getX();
+        int playerY = Globals.PLAYER.getY();
+
+        int newY = playerY;
+        int newX = playerX;
+        switch (direction) {
+            case Constants.DIRECTION_NORTH:
+                newY = playerY - 1;
+                break;
+            case Constants.DIRECTION_SOUTH:
+                newY = playerY + 1;
+                break;
+            case Constants.DIRECTION_EAST:
+                newX = playerX + 1;
+                break;
+            case Constants.DIRECTION_WEST:
+                newX = playerX - 1;
+                break;
+        }
+
+        Cell cell = cells[newX][newY];
+        if (cell.hasItem()) {
+            System.out.println("Interacting with: " + cell.getItem().toString());
+        }
+
+    }
 }
