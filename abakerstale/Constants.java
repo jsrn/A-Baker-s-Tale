bb2d7af66261f3bf01e10518af3a4c81a71e8189
James Nutt <james.srn@gmail.com>
2013-04-26 15:18:37 +0100

    added tile width, removed window modes

--- abakerstale/Constants.java
+++ abakerstale/Constants.java
@@ -4,11 +4,10 @@ import java.awt.Color;
 
 public class Constants {
     // Window modes
-    public static final int WINDOW_NORMAL = 1;
-    public static final int WINDOW_INVENTORY = 2;
     public static final int WINDOW_HEIGHT = 15;
     public static final int WINDOW_WIDTH = 20;
     public static final int FRAMES_PER_SECOND = 60;
+    public static final int TILE_WIDTH = 25;
     // Mobiles
     public static final int DIRECTION_NORTH = 1;
     public static final int DIRECTION_EAST = 2;
