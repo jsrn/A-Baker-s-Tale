2ee168fc7c6efcdae9d4fca8a85df35c35f4b2ee
James Nutt <james.srn@gmail.com>
2013-05-11 15:26:48 +0100

    load level from class

--- world/Screen.java
+++ world/Screen.java
@@ -1,38 +1,29 @@
 package world;
 
-import com.google.gson.Gson;
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.util.Iterator;
 import java.util.List;
+import levels.BaseLevel;
 
 public class Screen {
 
     private Cell[][] cells = new Cell[20][15];;
 
-    public Screen(String id) {
-
-        String level = readFile("src/levels/" + id);
+    public Screen(BaseLevel level) {
         
-        // Now do the magic.
-        ScreenJSON screenJSON = new Gson().fromJson(level, ScreenJSON.class);
+        int [] layer1 = level.getLayerOne();
+        int [] layer2 = level.getLayerTwo();
+        int [] passables = level.getPassableLayer();
         
-        System.out.println(screenJSON);
-                
-        List<Item> items = screenJSON.getItems();
-        
-        for (int x = 0; x < 20; x++) {
-            for (int y = 0; y < 15; y++) {
+        int count = 0;
+   
+        for (int y = 0; y < 15; y++) {
+            for (int x = 0; x < 20; x++) {
                 Cell c = new Cell();
+                c.setLayerOne(layer1[count]);
+                c.setLayerTwo(layer2[count]);
+                c.setPassable(passables[count] == 1);
                 cells[x][y] = c;
-            }
-        }
-        
-        for (Iterator<Item> it = items.iterator(); it.hasNext();) {
-            Item item = it.next();
-            addItemTile(item, item.getX(), item.getY());
-            if(!item.isPassable()){
-                cells[item.getX()][item.getY()].setPassable(false);
+                count++;
             }
         }
     }
@@ -48,24 +39,5 @@ public class Screen {
 
     public Cell[][] getCells() {
         return cells;
-    }
-
-    private String readFile(String file) {
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(file));
-            String line = null;
-            StringBuilder stringBuilder = new StringBuilder();
-            String ls = System.getProperty("line.separator");
-
-            while ((line = reader.readLine()) != null) {
-                stringBuilder.append(line);
-                stringBuilder.append(ls);
-            }
-
-            return stringBuilder.toString();
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-            return "";
-        }
     }
 }
