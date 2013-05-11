3d336e158996562631c90b838b243d6001b7aec5
James Nutt <james.srn@gmail.com>
2013-05-11 15:29:17 +0100

    match tiles to ids

---
+++ abakerstale/TileMap.java
@@ -0,0 +1,17 @@
+package abakerstale;
+
+import tiles.*;
+
+public class TileMap {
+
+    public static final BaseTile[] layerOne = {
+        null, //0
+        new Grass1() // 1
+    };
+    
+    public static final BaseTile[] layerTwo = {
+        null, // 0
+        new Boulder1() // 1
+    };
+    
+}
