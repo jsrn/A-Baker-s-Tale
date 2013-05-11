c9d02d3aec9e6c0a1919fb88f5f5395d15757cfe
James Nutt <james.srn@gmail.com>
2013-05-11 20:14:13 +0100

    tilemap

--- abakerstale/Globals.java
+++ abakerstale/Globals.java
@@ -8,16 +8,17 @@ import world.World;
 public class Globals {
     
     public static World WORLD;
+    
     public static Player PLAYER;
     
     public static boolean [] pressedKeys = new boolean[255];
-    
-    public static StaticTiles staticAssets = new StaticTiles();
-    
+        
     public static JPanel gamePanel;
     
     public static StateMachine stateMachine;
     
     public static int frameCount = 1;
+    
+    public static TileMap TILEMAP;
     
 }
